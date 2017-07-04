b5739b0b302759507d82e07bf42713a157786334
김택현 <taekbari157@gmail.com>
2017-07-04 17:25:49 +0900

    Added README.md file and added Lisence comment in js file

--- 04_Quotes_Rotator/js/jquery.cbpQTRotator.js
+++ 04_Quotes_Rotator/js/jquery.cbpQTRotator.js
@@ -1,3 +1,13 @@
+/**
+ * jquery.cbpQTRotator.js v1.0.0
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * Copyright 2013, Codrops
+ * http://www.codrops.com
+ */
 ;(function($, window, undefined) {
   'use strict';
 
